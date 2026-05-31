37796695b7bf5cfc711743626245c30378aafeee
Your Name <your-email@example.com>
2026-05-31 20:54:11 +0530

    Fix base path to /lord-harish/ for project repository

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const base = '/';
+const base = '/lord-harish/';
 
 export default defineConfig({
   plugins: [react()],
